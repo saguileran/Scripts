5a302e2019531534b0a2c5118dc92be772f07bb0
Sebas <saguileran@unal.edu.co>
2019-06-05 07:27:49 -0500

    comentando ehternet

--- Ethernet.py
+++ Ethernet.py
@@ -6,3 +6,4 @@ external=
 /sbin/iptables -t nat -A POSTRUTING-o $external -j MASQUERADE
 /sbin/iiptables -A FORWARD -i $external -o $internal -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/iptables -A FORWARD -i $internal -o $external -j ACCEPT
+#Comentario
